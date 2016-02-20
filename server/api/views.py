ff12e116411ec6fbd238df04eaf6bebbcadd64a7
Vincenzo Pandolfo <pandolfovince@gmail.com>
2016-02-20 15:55:13 +0000

    add implemented

--- server/api/views.py
+++ server/api/views.py
@@ -1,18 +1,27 @@
-import json
 import requests
 
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
-ELASTIC_URL = "https://search-getridofyoshit-mwboftivifwhy2cwtkdqnsc2fy.\
-        eu-west-1.es.amazonaws.com/"
-INDEX_TYPE = "freestuff/stuff/"
+ELASTIC_URL = (
+    "https://search-getridofyoshit-mwboftivifwhy2cwtkdqnsc2fy."
+    "eu-west-1.es.amazonaws.com/"
+)
+INDEX_TYPE = "test/stuff/"
 
 
+@csrf_exempt
 def add(req):
-    return HttpResponse("This is an add request")
+    res = requests.post(
+        ELASTIC_URL + INDEX_TYPE,
+        data=req.body.decode("utf-8")
+    )
+
+    return HttpResponse(res.text)
 
 
+@csrf_exempt
 def search(req):
     return HttpResponse("This is a search request")
