adb090a2262a697fec874829c0529267628a869c
Vincenzo Pandolfo <pandolfovince@gmail.com>
2016-02-20 15:16:48 +0000

    elastic url

--- server/api/views.py
+++ server/api/views.py
@@ -1,8 +1,13 @@
 import json
+import requests
 
 from django.http import HttpResponse
 
 # Create your views here.
+
+ELASTIC_URL = "https://search-getridofyoshit-mwboftivifwhy2cwtkdqnsc2fy.\
+        eu-west-1.es.amazonaws.com/"
+INDEX_TYPE = "freestuff/stuff/"
 
 
 def add(req):
