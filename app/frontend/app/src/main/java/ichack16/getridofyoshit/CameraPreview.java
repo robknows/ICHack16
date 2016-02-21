f6b82109fae4d9767b84f9d7f90e8a8a15a0e0e9
Zhou Fangyi <fangyi@Fangyi.local>
2016-02-21 01:29:51 +0000

    couple refactors on CameraPreview

--- app/frontend/app/src/main/java/ichack16/getridofyoshit/CameraPreview.java
+++ app/frontend/app/src/main/java/ichack16/getridofyoshit/CameraPreview.java
@@ -65,7 +65,6 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         try {
             mCamera.setPreviewDisplay(mHolder);
             mCamera.startPreview();
-
         } catch (Exception ignored) {
         }
     }
