7903b65d372474bdcd195d7b103ea4e21fa9ea74
Zhou Fangyi <fangyi@Fangyi.local>
2016-02-21 01:26:13 +0000

    refactoring QueryServer. fuk u vince

--- app/frontend/app/src/main/java/ichack16/getridofyoshit/api/QueryServer.java
+++ app/frontend/app/src/main/java/ichack16/getridofyoshit/api/QueryServer.java
@@ -2,11 +2,13 @@ package ichack16.getridofyoshit.api;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +18,7 @@ import org.json.JSONObject;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
 import android.util.Base64;
 
 import ichack16.getridofyoshit.FreeStuff;
@@ -34,42 +37,41 @@ public class QueryServer {
   public String addStuff(FreeStuff newStuff) {
     try {
       URL addURL = new URL(serverURL + ADD_API);
-      HttpURLConnection conn = (HttpURLConnection) addURL.openConnection();
+      HttpURLConnection connection = (HttpURLConnection) addURL.openConnection();
 
-      conn.setRequestMethod("POST");
-      conn.setDoInput(true);
-      conn.setDoOutput(true);
-      conn.setReadTimeout(10000);
-      conn.setConnectTimeout(15000);
+      setupConnection(connection);
 
-      OutputStream out = conn.getOutputStream();
-      BufferedWriter writer = new BufferedWriter(
-          new OutputStreamWriter(out, "UTF-8"));
+      OutputStream out = connection.getOutputStream();
+      BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
 
       writer.write(newStuff.toString());
       writer.flush();
       writer.close();
 
-      conn.connect();
+      connection.connect();
 
-      return conn.getResponseMessage();
+      return connection.getResponseMessage();
     } catch (Exception e) {
       return "FAIL";
     }
   }
 
+  private void setupConnection(HttpURLConnection conn) throws ProtocolException {
+    conn.setRequestMethod("POST");
+    conn.setDoInput(true);
+    conn.setDoOutput(true);
+    conn.setReadTimeout(10000);
+    conn.setConnectTimeout(15000);
+  }
+
   private String queryClose(Location location) {
     try {
       URL addURL = new URL(serverURL + SEARCH_API);
-      HttpURLConnection conn = (HttpURLConnection) addURL.openConnection();
+      HttpURLConnection connection = (HttpURLConnection) addURL.openConnection();
 
-      conn.setRequestMethod("POST");
-      conn.setDoInput(true);
-      conn.setDoOutput(true);
-      conn.setReadTimeout(10000);
-      conn.setConnectTimeout(15000);
+      setupConnection(connection);
 
-      OutputStream out = conn.getOutputStream();
+      OutputStream out = connection.getOutputStream();
       BufferedWriter writer = new BufferedWriter(
           new OutputStreamWriter(out, "UTF-8"));
 
@@ -77,57 +79,54 @@ public class QueryServer {
       writer.flush();
       writer.close();
 
-      conn.connect();
+      connection.connect();
 
-      InputStream in = conn.getInputStream();
+      InputStream in = connection.getInputStream();
       BufferedReader read = new BufferedReader(new InputStreamReader(in, "UTF-8"));
 
-      StringBuilder response = new StringBuilder();
-
-      String line;
-
-      while ( (line = read.readLine()) != null) {
-        response.append(line);
-      }
-
-      return response.toString();
-
+      return buildResponse(read);
     } catch (Exception e) {
       e.printStackTrace();
       return ""; 
     }
   }
 
-  public List<FreeStuff> getClose(Location location) {
+  @NonNull
+  private String buildResponse(BufferedReader read) throws IOException {
+    StringBuilder response = new StringBuilder();
+    String line;
+    while ( (line = read.readLine()) != null) {
+      response.append(line);
+    }
+    return response.toString();
+  }
+
+  public List<FreeStuff> freeStuffNearTo(Location location) {
     List<FreeStuff> found = new ArrayList<FreeStuff>();
 
-    String res = queryClose(location);
-
     try {
-      JSONObject jsonObject = new JSONObject(res);
-      JSONObject hits = jsonObject.getJSONObject("hits");
-      JSONArray actualhits = hits.getJSONArray("hits");
+      JSONObject jsonObject = new JSONObject(queryClose(location));
+      String KEY_FOR_RESULTS = "hits";
+      JSONObject hits = jsonObject.getJSONObject(KEY_FOR_RESULTS);
+      JSONArray actualHits = hits.getJSONArray(KEY_FOR_RESULTS);
 
-      for (int i = 0; i < actualhits.length(); i++) {
-        JSONObject obj = actualhits.getJSONObject(i);
+      for (int i = 0; i < actualHits.length(); i++) {
+        JSONObject obj = actualHits.getJSONObject(i);
         JSONObject data = obj.getJSONObject("_source");
 
         String description = data.getString("description");
         String name = data.getString("name");
         String telephoneNumber = data.getString("telephoneNumber");
 
-        JSONObject jsonlocation = data.getJSONObject("location");
+        JSONObject jsonLocation = data.getJSONObject("location");
 
-        Location thisLocation = new Location(
-            jsonlocation.getDouble("lat"), jsonlocation.getDouble("lon"));
+        Location thisLocation = new Location(jsonLocation.getDouble("lat"), jsonLocation.getDouble("lon"));
 
-        byte[] decodedimage = Base64.decode(data.getString("image"), 0);
+        byte[] decodedImage = Base64.decode(data.getString("image"), 0);
 
-        Bitmap image = BitmapFactory.decodeByteArray(
-            decodedimage, 0, decodedimage.length);
+        Bitmap image = BitmapFactory.decodeByteArray(decodedImage, 0, decodedImage.length);
 
-        found.add(new FreeStuff(
-              image, name, description, telephoneNumber, thisLocation));
+        found.add(new FreeStuff(image, name, description, telephoneNumber, thisLocation));
       }
     } catch (Exception e) {
       e.printStackTrace();
